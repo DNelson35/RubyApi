5dc9a65cc786c556fd09af6ab16f10eb0b0a60cc
Damien Nelson <dhnisaman@gmail.com>
2023-04-21 23:23:59 -0400

    create companies table

---
+++ db/migrate/20230417183806_create_companies.rb
@@ -0,0 +1,9 @@
+class CreateCompanies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.string :logo_url
+      t.timestamps
+    end
+  end
+end
